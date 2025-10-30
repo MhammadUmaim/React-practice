0f1dcdaec5238fadaa2e759f86d76f3b7ad588c4
MhammadUmaim <muhammadumaim7@gmail.com>
2025-10-30 20:32:36 +0500

    started practicing react

---
+++ react-practice/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <>
+    <div>
+      <h1>hello</h1>
+      <h3>Umaim</h3>
+    </div>
+    <a href="#">click me</a>
+    <a href="#">click me</a>
+    </>
+  )
+}
+
+export default App
